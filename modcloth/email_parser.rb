20d4cc593a30437c84637117bbdec7db642a28a5
Nicholas <nick@targetmobi.com>
2012-01-20 21:05:03 -0600

    added instructions

--- modcloth/email_parser.rb
+++ modcloth/email_parser.rb
@@ -1,2 +1,5 @@
 #!/usr/bin/env ruby
+
+# You are given several strings that may/may not be valid emails. You should write a regular expression that determines if the email id is a valid email id or not. You may assume all characters are from the english language.
+
 File.readlines(ARGV[0]).each{ |string| puts ( string =~ /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i ? true : false ) }
